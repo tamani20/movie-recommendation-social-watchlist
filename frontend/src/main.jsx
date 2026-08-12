86b4fe569636d837e024259e29f80427c8628567
Tamani <tamani@umich.edu>
2026-08-12 03:40:15 -0400

    Add global UI styling system

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
